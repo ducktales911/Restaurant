09f8bf66e5067a28978ff8c07f5b1923a0894c70
Thomas Hamburger <hamburgerthomas@gmail.com>
2018-12-11 21:01:44 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Thomas Hamburger on 11/12/2018.
+//  Copyright © 2018 Thomas Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
